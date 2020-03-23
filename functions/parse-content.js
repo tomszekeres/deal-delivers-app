3b01671883a54702656587c2626510761132a832
Rob Hough <robhough180@gmail.com>
2020-03-23 12:03:08 +0000

    added json function, netlify cms config

---
+++ functions/parse-content.js
@@ -0,0 +1,65 @@
+const fs = require('fs')
+const path = require('path')
+const jsonConcat = require('json-concat')
+const _set = require('lodash/set')
+const _mergeWith = require('lodash/mergeWith')
+const _isArray = require('lodash/isArray')
+const globCb = require('glob')
+const util = require('util')
+
+const glob = util.promisify(globCb)
+const readFile = util.promisify(fs.readFile)
+
+const options = {
+  contentDir: './content/',
+  outputFile: './src/data.json'
+}
+
+const getCollectionType = filePath => {
+  const pathParsed = path.parse(filePath)
+  const objectKey = pathParsed.dir
+    .replace(options.contentDir, '')
+    .replace(/\//g, '.')
+  return `${objectKey}`
+}
+
+const getDocumentName = filePath => {
+  const pathParsed = path.parse(filePath)
+  return `${pathParsed.name}`
+}
+
+const getDocumentExt = filePath => {
+  const pathParsed = path.parse(filePath)
+  return `${pathParsed.ext}`
+}
+
+const getFileContents = filePath => {
+  return readFile(filePath, 'utf8').then(data => {
+    let documentData = JSON.parse(data)
+    documentData.id = getDocumentName(filePath)
+    documentData.body = documentData.body || documentData.content
+    console.log(`✨  Processed ${filePath}`)
+    return documentData
+  })
+}
+
+const readFiles = async paths => Promise.all(paths.map(getFileContents))
+
+// Combines all JSON Together
+const combineJSON = async () => {
+  console.log(`✨Reading JSON files in ${options.contentDir}`)
+  const paths = await glob(`${options.contentDir}/**/**.json`)
+  const results = await readFiles(paths)
+  const data = [...results]
+  return JSON.stringify(data, null, 2)
+}
+
+// Creates data file
+const writeJSON = async () => {
+  const json = await combineJSON()
+  fs.writeFileSync(options.outputFile, json)
+  console.log(`✅Data saved to ${options.outputFile}`)
+  process.exit()
+}
+
+writeJSON();
