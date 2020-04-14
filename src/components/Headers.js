c637918afba949d52ab5004468d504555d5f1187
Tom Szekeres <tom@szeker.es>
2020-04-14 18:34:48 +0100

    Added brand colours and updated intro text

--- src/components/Headers.js
+++ src/components/Headers.js
@@ -8,7 +8,7 @@ export const Nav = () => {
   return (
     <StyledNav role="navigation">
       <Button path="https://forms.gle/9MdrFgRAueyUNkSWA">
-        📍 List your location
+        📍 List your business
       </Button>
        <Button path="mailto:tom@dulwichdelivers.com">
         📧 Email us
@@ -22,7 +22,7 @@ export const HeroHeader = ({ search }) => {
     <StyledHeroHeader role="banner" style={{ backgroundImage: `url(/images/Header@2x.png)` }}>
       <Container>
         <h1>Dulwich Delivers</h1>
-        <p>Helping local restaurants, shops & essential services that deliver direct to your door. Search for something e.g. coffee, ice cream:</p>
+        <p>Helping local restaurants, shops & essential services that deliver direct to your door. Try searching what you're looking for e.g. coffee, ice cream:</p>
         <LocationSearch />
       </Container>
     </StyledHeroHeader >
